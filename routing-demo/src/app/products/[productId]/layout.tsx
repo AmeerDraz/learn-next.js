c453b88d0f1062bd5331ef1e518492f6f420623b
AmeerDraz <ameerabudraz1@gmail.com>
2025-11-17 12:08:48 +0200

    learn layout and multiple layout

---
+++ routing-demo/src/app/products/[productId]/layout.tsx
@@ -0,0 +1,11 @@
+export default function ProductsListLayout(
+    
+    {children} : {children: React.ReactNode}
+    
+) {
+    return <>
+    {children}
+    
+    <h2>Featured product</h2>
+    </> 
+}
